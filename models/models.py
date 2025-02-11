f5c89cab90fe6d16df768e25d12b4235c9b76ada
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-02-11 21:29:30 +0100

    fix problem with help information

--- models/models.py
+++ models/models.py
@@ -15,15 +15,15 @@ class AudioModel(Base):
     name: Mapped[str] = mapped_column()
     file_path: Mapped[str] = mapped_column()
     time_stamp: Mapped[str] = mapped_column(
-        help="This field represents the time stamp which it is separated by a comma"
+        comment="This field represents the timestamp, separated by a comma"
     )
     added: Mapped[datetime] = mapped_column(server_default=func.now())
     duration: Mapped[int] = mapped_column(
-        default=0, help="Duration of the audio in seconds"
+        default=0, comment="Duration of the audio in seconds"
     )
     spend_time: Mapped[int] = mapped_column(
-        default=0, help="How many seconds the audio was played"
+        default=0, comment="Total time the audio was played, in seconds"
     )
     finished_times: Mapped[int] = mapped_column(
-        default=0, help="How many times the audio was finished"
+        default=0, comment="Number of times the audio was finished"
     )
