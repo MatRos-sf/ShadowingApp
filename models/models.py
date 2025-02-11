649b77c8925181ed5ea7918da173f997655da464
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-02-11 21:18:21 +0100

    implemented db

--- models/models.py
+++ models/models.py
@@ -1,6 +1,8 @@
 """SQL models for the database."""
 
-from sqlalchemy import DateTime, SmallInteger, func
+from datetime import datetime
+
+from sqlalchemy import func
 from sqlalchemy.orm import Mapped, mapped_column
 
 from .db_setup import Base
@@ -13,7 +15,7 @@ class AudioModel(Base):
     name: Mapped[str] = mapped_column()
     file_path: Mapped[str] = mapped_column()
     time_stamp: Mapped[str] = mapped_column()
-    added: Mapped[DateTime] = mapped_column(default=func.now())
+    added: Mapped[datetime] = mapped_column(server_default=func.now())
     duration: Mapped[int] = mapped_column(default=0)
     spend_time: Mapped[int] = mapped_column(default=0)
-    finished_times: Mapped[SmallInteger] = mapped_column(default=0)
+    finished_times: Mapped[int] = mapped_column(default=0)
