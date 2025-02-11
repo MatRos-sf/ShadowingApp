856af76b3ea6bb3354e44218d501cda365fdb653
MatRos-sf <mateuszrosenkranz@gmail.com>
2025-02-11 20:53:04 +0100

    create base model to Audio handle

--- models/models.py
+++ models/models.py
@@ -1,1 +1,21 @@
 """SQL models for the database."""
+
+from sqlalchemy import DateTime, SmallInteger, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class AudioModel(Base):
+    __tablename__ = "audio"
+
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column()
+    file_path: Mapped[str] = mapped_column()
+    time_stamp: Mapped[str] = mapped_column()
+    added: Mapped[DateTime] = mapped_column(default=func.now())
+    duration: Mapped[int] = mapped_column(default=0)
+    spend_time: Mapped[int] = mapped_column(default=0)
+    finished_times: Mapped[SmallInteger] = mapped_column(default=0)
